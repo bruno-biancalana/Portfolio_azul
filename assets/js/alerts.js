a45bad2aec4e7d2241c4e91bf816a3dd1a665d6e
bruno-biancalana <bruno.biancalana@outlook.com>
2022-12-25 13:39:49 -0300

    Inclui pop-ups de sucesso + altera projetos eng

---
+++ assets/js/alerts.js
@@ -0,0 +1,21 @@
+function popuppt() {
+    Swal.fire({
+        position: 'top-center',
+        icon: 'success',
+        title: 'Mensagem enviada com sucesso! Em breve entrarei em contato ;) ',
+        showConfirmButton: false,
+        timer: 3000
+    })
+}
+
+
+
+function popupen() {
+    Swal.fire({
+        position: 'top-center',
+        icon: 'success',
+        title: 'Message sent successfully! I will contact you soon ;) ',
+        showConfirmButton: false,
+        timer: 3000
+    })
+}
