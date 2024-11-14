3b7059e421a57520ddfa6196eac715f5bb611b14
Bruno Biancalana <bruno.biancalana@outlook.com>
2024-11-14 12:53:26 -0300

    perf - unify js archives

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
+/* ALERTS ESTILIZADOS, PÓS ENVIO DO FORMULÁRIO */
+
+function popuppt() {
+    Swal.fire({
+        position: 'top-center',
+        icon: 'success',
+        title: 'Mensagem enviada com sucesso! Em breve entrarei em contato ;) ',
+        showConfirmButton: false,
+        timer: 3000
+    })
+}
+
+function popupen() {
+    Swal.fire({
+        position: 'top-center',
+        icon: 'success',
+        title: 'Message sent successfully! I will contact you soon ;) ',
+        showConfirmButton: false,
+        timer: 3000
+    })
+}
 document.addEventListener('DOMContentLoaded', () => {
     
     const trackableButtons = document.querySelectorAll('[data-track]');
